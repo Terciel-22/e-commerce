ae624609dbedbe37901057a7d78554e82857c3c1
Terciel-22 <dietherfiel@gmail.com>
2022-12-31 02:50:58 +0800

    Added top-navigation bar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+function App() {
+    return (
+        <>
+            <Navbar appName="Terciel" />
+            <div className="bg-dark text-white">Hello World</div>
+        </>
+    );
+}
+
+export default App;
