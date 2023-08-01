7df2982f8960ee6ea0268b27e04da05a945be64f
Ter C. Fiel <120294370+Terciel-22@users.noreply.github.com>
2023-08-01 12:53:47 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ class Navbar extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
                 <div className="container">
-                    <a className="navbar-brand" href="/home">
+                    <a className="navbar-brand" href="/">
                         <FontAwesomeIcon icon={faShoppingBag} className="me-2" />
                         {this.props.appName}
                     </a>
